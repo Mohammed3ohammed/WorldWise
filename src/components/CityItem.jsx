2a32cb3f4b5a3e356b82e14d2c316a5533aaf7fa
Mohammed <mohammed@gmail.com>
2025-05-04 10:27:06 -0700

    update

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -3,21 +3,21 @@ import { styles } from 'Spinner.module.css';
     const formatDate = (date) => {
         new Intl.DateTimeFormat("en", {
             day: "numeric",
-            month: "long",
+            month: "long",  
             year: "numeric",
         }).format(new Date(date))
     }
 
-function CityItem() {
-    const {cityName, emoji, data} = city;
+function CityItem({city} ) {
+    const { cityName, emoji, date } = city;
     return (
         <li className={styles.cityItems}>
             <span className={styles.emoji}>{emoji}</span>
             <h3 className={styles.name}>{cityName}</h3>
-            <time className={styles.date}>{data}</time>
+            <time className={styles.date}>({formatDate})</time>
             <button className={styles.deleteBtn}>&times;</button>
         </li>
-    )
+    );
 }
 
 export default CityItem;
