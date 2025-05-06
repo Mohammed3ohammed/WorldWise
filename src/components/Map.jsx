b16453ee70e4c31f25652f2375576ed1ffe437e2
Mohammed <mohammed@gmail.com>
2025-05-06 12:09:00 -0700

    feat: add Map component with ability to update position

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import styles from "./Map.module.css";
 
 function Map() {
+    const [searchParams, setSearchParams] = useState();
+    const lat = searchParams.get("lat");
+    const lng = searchParams.get("lng");
+    
     return (
         <div className={styles.mapContainer}>
-        map 
+        <h1>Map</h1>
+        <h2>Poition: {lat}, {lng}</h2>
+        <button
+        onClick={() => {
+            setSearchParams({lat: 23, lng: 50})
+        }}
+        >Chinge pos</button>
         </div>
     )
 }
