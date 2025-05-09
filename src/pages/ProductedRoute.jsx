7f6d9b80b8ea0e242bf26b4554da41c2ab7273d4
Mohammed <mohammed@gmail.com>
2025-05-09 15:13:43 -0700

    Set up authentication context with login and logout functionality

---
+++ src/pages/ProductedRoute.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/";
+
+
+function ProductedRoute( {children} ) {
+    const {isAuthenticated} = useAuth();
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!isAuthenticated)  navigate("/");
+        },
+     [isAuthenticated, navigate]
+    );
+    return isAuthenticated ? children : null;
+}
+
+export default ProductedRoute;
