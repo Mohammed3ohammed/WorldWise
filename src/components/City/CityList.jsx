c1d95e1554342594834c93ef416657a745189cd3
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-07-02 06:06:33 +0300

    Fix: handle null city data and improve UI in CityItem

--- src/components/City/CityList.jsx
+++ src/components/City/CityList.jsx
@@ -8,8 +8,9 @@ function CityList()  {
 
     const { cities, isLoading  } = useCities();
 
-    if (isLoading) return <Spinner />
-    if(!cities.length) return <Message message="Add your first city by clicking on a city on the map" />
+    if (isLoading) return <Spinner />;
+
+    if (!cities.length) return <Message message="Add your first city by clicking on a city on the map" />
 
     return (
         <ul className={styles.cityList}>
--- src/components/Country/CountryItem.jsx
+++ src/components/Country/CountryItem.jsx
@@ -1,6 +1,13 @@
+import styles from "./CountryItem.module.css";
+
 
 function CountryItem() {
-
+    return (
+        <li className={styles.CountryItem}>
+            <span>{country.emoji}</span>
+            <span>{country.country}</span>
+        </li>
+    );
 }
 
 export default CountryItem;
