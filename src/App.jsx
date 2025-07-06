151d0db573d9cc5feaecd9757b48b6b8c15a1aff
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-07-06 09:24:51 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { lazy, Suspense } from "react";
 import Login from "./pages/Login/Login";
 import { AuthProvider } from "./contexts/FakeAuthContext";
 import CityList from "./components/City/CityList";
+import City from './components/City/City'
 import CountriesList from './components/Country/CountryList';
 import Form from "./components/Form/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
@@ -12,7 +13,7 @@ import ProtectedRoute from './pages/ProtectedRoute'
   const Homepage = lazy(() => import("./pages/Homepage/Homepage"));
   const Product = lazy(() => import("./pages/Product/Product"));
   const Pricing = lazy(() => import("./pages/Pricing"));
-  const AppLayout = lazy(() => import("./pages/AppLayout/AppLayout"));
+  const AppLayout = lazy(() => import("./pages/AppLayout"));
   const PageNotfound = lazy( () => import('./pages/PageNotFound'));
 
 
@@ -23,19 +24,24 @@ import ProtectedRoute from './pages/ProtectedRoute'
         <CitiesProvider>
           <BrowserRouter>
           <Suspense fallback={<SpinnerFullPage />}>
-      <Routes>
+        
+        <Routes>
         <Route  index element={<Homepage />}  />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={
-          <ProtectedRoute>
-          <AppLayout />
-          </ProtectedRoute>
-          }>
-            <Route index element={<Navigate replace to="cities" />} />
-        <Route index element={<CityList  />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+          
+          <Route index element={<Navigate replace to="cities" />} />
         <Route path="cities" element={<CityList  />} />
+        <Route path="cities/:id" element={<City />} />
         <Route path="countries" element={<CountriesList  />} />
         <Route path="form" element={<Form />} />
         </Route>
